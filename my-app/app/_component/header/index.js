959211cd354a9a92b0737a93ba60defcaa96eb9d
wabi172 <114135493+wabi172@users.noreply.github.com>
2025-04-10 13:19:30 +0800

    U

---
+++ my-app/app/_component/header/index.js
@@ -0,0 +1,11 @@
+'use client'
+import React, { useState,useEffect } from "react"
+
+
+export default function Header() {
+    const [user, setUser] = useState()
+
+    return(
+        <></>
+    )
+}
